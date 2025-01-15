344dfbd27e3a7b0db3b9742ed0131e9d1703e626
shazaniyu55 <shazaniyu@gmail.com>
2025-01-15 05:29:06 +0100

    game

--- src/Components/RiskAssessment/main.tsx
+++ src/Components/RiskAssessment/main.tsx
@@ -1,81 +1,130 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 import '../generalStye.css';
 import Bold from '@/utils/Bold';
 import ResponsiveImage from '@/utils/ResposiveImage';
 import { MDBBtn } from 'mdb-react-ui-kit';
 import { Alert } from '@mui/material';
-import AssessmentResult from './AssessmentResult';
-import useInnerWidth from '../pub/Hooks/useInnerWidth';
-import {
-    MDBModal,
-    MDBModalDialog,
-    MDBModalContent,
-    MDBModalHeader,
-    MDBModalTitle,
-    MDBModalBody,
-    MDBModalFooter,
-  } from 'mdb-react-ui-kit';
+import dynamic from 'next/dynamic';
+
+// Dynamically import MDBModal to prevent SSR issues
+const MDBModal = dynamic(() =>
+  import('mdb-react-ui-kit').then((mod) => mod.MDBModal), { ssr: false });
+const MDBModalDialog = dynamic(() =>
+  import('mdb-react-ui-kit').then((mod) => mod.MDBModalDialog), { ssr: false });
+const MDBModalContent = dynamic(() =>
+  import('mdb-react-ui-kit').then((mod) => mod.MDBModalContent), { ssr: false });
+const MDBModalHeader = dynamic(() =>
+  import('mdb-react-ui-kit').then((mod) => mod.MDBModalHeader), { ssr: false });
+const MDBModalTitle = dynamic(() =>
+  import('mdb-react-ui-kit').then((mod) => mod.MDBModalTitle), { ssr: false });
+const MDBModalBody = dynamic(() =>
+  import('mdb-react-ui-kit').then((mod) => mod.MDBModalBody), { ssr: false });
+const MDBModalFooter = dynamic(() =>
+  import('mdb-react-ui-kit').then((mod) => mod.MDBModalFooter), { ssr: false });
+
 import AssessmentInstructions from './instructions';
 import AssessmentGame from './AssessmentGame';
+import useInnerWidth from '../pub/Hooks/useInnerWidth';
 
+const Main: React.FC = () => {
+  const [start, setStart] = useState<boolean>(false);
+  const [width, setWidth] = useState<number | null>(null); // Initialize width as null
+  const [basicModal, setBasicModal] = useState<boolean>(false);
 
-const Main:React.FC=()=>{
-    const width=useInnerWidth();
-const [start,setStart]=useState<boolean>(false);
-    const [basicModal, setBasicModal] = useState(width < 1200);
+  useEffect(() => {
+    // Check if the window object is available
+    if (typeof window !== 'undefined') {
+      setWidth(window.innerWidth);
+      setBasicModal(window.innerWidth < 1200);
+    }
+  }, []);
 
   const toggleOpen = () => setBasicModal(!basicModal);
-    return (
-        <>
-         
-      <MDBModal open={basicModal} onClose={() => setBasicModal(false)} tabIndex='-1'>
+
+  if (width === null) {
+    return null; // Render nothing until the window width is determined
+  }
+
+  return (
+    <>
+      <MDBModal open={basicModal} onClose={() => setBasicModal(false)} tabIndex="-1">
         <MDBModalDialog>
           <MDBModalContent>
             <MDBModalHeader>
               <MDBModalTitle>Risk Assessment Instructions</MDBModalTitle>
-              <MDBBtn className='btn-close' color='none' onClick={toggleOpen}></MDBBtn>
+              <MDBBtn className="btn-close" color="none" onClick={toggleOpen}></MDBBtn>
             </MDBModalHeader>
-            <MDBModalBody><AssessmentInstructions/></MDBModalBody>
-
+            <MDBModalBody>
+              <AssessmentInstructions />
+            </MDBModalBody>
             <MDBModalFooter>
-              <MDBBtn color='secondary' onClick={toggleOpen}>
+              <MDBBtn color="secondary" onClick={toggleOpen}>
                 Close
               </MDBBtn>
             </MDBModalFooter>
           </MDBModalContent>
         </MDBModalDialog>
       </MDBModal>
-         <div style={{padding:16,borderRight:'1px solid lightgrey'}}>
-         {!start ?<>
-          <div className='myContainer'>
-           <div className='d-flex justify-content-center' style={{maxHeight:width<700?170:250}}><ResponsiveImage containerSize={width <700 ? 200:300} src='/images/girlHeartDevice.png'/></div>
-<Bold style={{fontSize:"large",textAlign:"center",display:"block"}}>Take your Hypertension Risk Assessment</Bold>
-<div className='d-flex align-items-center justify-content-center' style={{marginTop:7}}>
-<Alert title='Note' color='info' style={{maxWidth:'500px'}}>
-Take Charge of Your Heart Health: Discover Your Hypertension Risk with Our Comprehensive Assessment. Get Personalized Insights and Recommendations to Manage and Reduce Your Risk Factors – Start Your Journey to Better Health Today!
-</Alert>
-</div>
 
-     <div className='d-flex align-items-center justify-content-center' style={{marginTop:8}}>
-     <MDBBtn style={{maxWidth:"200px",width:"100%"}} rounded color='dark' onClick={()=>{
-        setStart(true);
-     }}>Get Started</MDBBtn>      </div>
-     </div>
-           <br/>
-           {width < 1200 && <div className='d-flex justify-content-center'><MDBBtn onClick={toggleOpen} color='link'>Show instructions</MDBBtn></div>}<br/>
-           </>:<>
-         <AssessmentGame/>
-         
-         {width < 1200 && <><br/><div className='d-flex justify-content-center'><MDBBtn onClick={toggleOpen} color='link'>Show instructions</MDBBtn></div></>}
-           </>
-        }
-
-           </div>
-           </>
-
-    )
-}
+      <div style={{ padding: 16, borderRight: '1px solid lightgrey' }}>
+        {!start ? (
+          <>
+            <div className="myContainer">
+              <div className="d-flex justify-content-center" style={{ maxHeight: width < 700 ? 170 : 250 }}>
+                <ResponsiveImage containerSize={width < 700 ? 200 : 300} src="/images/girlHeartDevice.png" />
+              </div>
+              <Bold style={{ fontSize: 'large', textAlign: 'center', display: 'block' }}>
+                Take your Hypertension Risk Assessment
+              </Bold>
+              <div className="d-flex align-items-center justify-content-center" style={{ marginTop: 7 }}>
+                <Alert title="Note" color="info" style={{ maxWidth: '500px' }}>
+                  Take Charge of Your Heart Health: Discover Your Hypertension Risk with Our Comprehensive Assessment.
+                  Get Personalized Insights and Recommendations to Manage and Reduce Your Risk Factors – Start Your
+                  Journey to Better Health Today!
+                </Alert>
+              </div>
+              <div className="d-flex align-items-center justify-content-center" style={{ marginTop: 8 }}>
+                <MDBBtn
+                  style={{ maxWidth: '200px', width: '100%' }}
+                  rounded
+                  color="dark"
+                  onClick={() => {
+                    setStart(true);
+                  }}
+                >
+                  Get Started
+                </MDBBtn>
+              </div>
+            </div>
+            <br />
+            {width < 1200 && (
+              <div className="d-flex justify-content-center">
+                <MDBBtn onClick={toggleOpen} color="link">
+                  Show instructions
+                </MDBBtn>
+              </div>
+            )}
+            <br />
+          </>
+        ) : (
+          <>
+            <AssessmentGame />
+            {width < 1200 && (
+              <>
+                <br />
+                <div className="d-flex justify-content-center">
+                  <MDBBtn onClick={toggleOpen} color="link">
+                    Show instructions
+                  </MDBBtn>
+                </div>
+              </>
+            )}
+          </>
+        )}
+      </div>
+    </>
+  );
+};
 
 export default Main;
-export {AssessmentResult}
--- src/Components/community/main.tsx
+++ src/Components/community/main.tsx
@@ -16,145 +16,125 @@ import useUser from '../hooks/useUser';
 import { closeAllMDBModal, sortObjectsByDate } from '@/Logics/date';
 import { MDBBtn } from 'mdb-react-ui-kit';
 import useInnerWidth from '../pub/Hooks/useInnerWidth';
+import dynamic from 'next/dynamic';
 
+// Dynamically import MDBModal components to prevent SSR issues
+const MDBModal = dynamic(() =>
+  import('mdb-react-ui-kit').then((mod) => mod.MDBModal), { ssr: false });
+const MDBModalDialog = dynamic(() =>
+  import('mdb-react-ui-kit').then((mod) => mod.MDBModalDialog), { ssr: false });
+const MDBModalContent = dynamic(() =>
+  import('mdb-react-ui-kit').then((mod) => mod.MDBModalContent), { ssr: false });
+const MDBModalHeader = dynamic(() =>
+  import('mdb-react-ui-kit').then((mod) => mod.MDBModalHeader), { ssr: false });
+const MDBModalTitle = dynamic(() =>
+  import('mdb-react-ui-kit').then((mod) => mod.MDBModalTitle), { ssr: false });
+const MDBModalBody = dynamic(() =>
+  import('mdb-react-ui-kit').then((mod) => mod.MDBModalBody), { ssr: false });
+const MDBModalFooter = dynamic(() =>
+  import('mdb-react-ui-kit').then((mod) => mod.MDBModalFooter), { ssr: false });
 
-import {
+const Main: React.FC = () => {
+  const width = useInnerWidth();
+  const [isLoading_, setIsLoading] = useState<boolean>(true);
+  const { user, isLoading } = useUser();
+  const [data, setData] = useState<Post[]>([]);
+  const [error, setError] = useState<string>();
+  const editorHeading = useSelector((state: RootState) => state.data.data);
 
-    MDBModal,
-    MDBModalDialog,
-    MDBModalContent,
-    MDBModalHeader,
-    MDBModalTitle,
-    MDBModalBody,
-    MDBModalFooter,
-  } from 'mdb-react-ui-kit';
-  
-
-const Main:React.FC=()=>{
-    const width=useInnerWidth()
-const [isLoading_,setIsLoading]=useState<boolean>(true);
-const {user,isLoading}=useUser()
-const [data,setData]=useState<Post[]>([]);
-const [error,setError]=useState<string>();
-const editorHeading = useSelector((state: RootState) => state.data.data);
-
-const getQuestions=async (user:any)=>{
+  const getQuestions = async (user: any) => {
     setIsLoading(true);
-try{
-    const data=await docQr("CommunityQuestions",{
-    
-    max:100,
-    whereClauses:[
-        {
-            field:"profile",
-            operator:"!=",
-            value:""
-        }
-    ]
-})
-setData(sortObjectsByDate(data).reverse());
-}
-catch(err:any){
-setError(err.error || "Something went wrong")
-}
-finally{
-    setIsLoading(false);
-}
-}
+    try {
+      const data = await docQr("CommunityQuestions", {
+        max: 100,
+        whereClauses: [
+          {
+            field: "profile",
+            operator: "!=",
+            value: ""
+          }
+        ]
+      });
+      setData(sortObjectsByDate(data).reverse());
+    } catch (err: any) {
+      setError(err.error || "Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
-const [BodySide_, setBodySide_] = useState<any>(<></>)
+  const [BodySide_, setBodySide_] = useState<any>(<></>);
 
+  useEffect(() => {
+    if (!isLoading && user) {
+      getQuestions(user);
+      dispatch(setEditorHeaderData("Create Post"));
+    } else {
+      console.log("no user", user);
+    }
 
-useEffect(()=>{
-if(!isLoading && user){
-    getQuestions(user)
-  dispatch(setEditorHeaderData("Create Post"));
-}
-    else{
-console.log("no user",user)
-}
+    const getComponent = async () => {
+      const { default: BodySide } = await import("../side/bodySide");
+      if (width < 1024) setBodySide_(<BodySide />);
+    };
+    getComponent();
 
-///get editor
-const getComponent = async () => {
-    const { default: BodySide } = await import("../side/bodySide");
-    if (width < 1024) setBodySide_(<BodySide />);
-}
-getComponent();
-setShowPostQuestion(false)
+    setShowPostQuestion(false);
 
+    setTimeout(() => {
+      closeAllMDBModal();
+    }, 800);
+  }, [user, isLoading, width]);
 
-setTimeout(()=>{
-      closeAllMDBModal()
-},800);
+  const dispatch: AppDispatch = useDispatch();
+  const [showPostQuestion, setShowPostQuestion] = useState<boolean>(false);
 
+  return (
+    <div className='dashboard' style={{ padding: 17 }}>
+      <h3 className='d-flex align-items-center justify-content-between'>
+        <Bold>Community Question</Bold> {width < 1024 && <MDBBtn color='dark' rounded size='sm' onClick={() => {
+          setShowPostQuestion(true);
+        }} >Post new</MDBBtn>}
+      </h3>
 
-
-},[]);
-
-
-
-
-
-  
-const dispatch: AppDispatch = useDispatch();
-
-const [showPostQuestion,setShowPostQuestion]=useState<boolean>(false);
-
-
-
-
-    return (
-        <div className='dashboard' style={{padding:17}}>
-            <h3 className='d-flex align-items-center justify-content-between'><Bold>Community Question</Bold> {width < 1024 && <MDBBtn color='dark' rounded size='sm' onClick={()=>{
-                setShowPostQuestion(true)
-            }} >Post new</MDBBtn>}</h3>
-          
-            <MDBModal className='mdb-modal' open={showPostQuestion} onClose={() => setShowPostQuestion(false)} >
+      <MDBModal className='mdb-modal' open={showPostQuestion} onClose={() => setShowPostQuestion(false)} >
         <MDBModalDialog>
           <MDBModalContent>
             <MDBModalHeader>
               <MDBModalTitle>{editorHeading}</MDBModalTitle>
-              <MDBBtn className='btn-close' color='none' onClick={()=>setShowPostQuestion(false)}></MDBBtn>
+              <MDBBtn className='btn-close' color='none' onClick={() => setShowPostQuestion(false)}></MDBBtn>
             </MDBModalHeader>
             <MDBModalBody>
-                {BodySide_}
-                </MDBModalBody>
-</MDBModalContent>
-</MDBModalDialog>
-</MDBModal>
+              {BodySide_}
+            </MDBModalBody>
+          </MDBModalContent>
+        </MDBModalDialog>
+      </MDBModal>
 
-            {isLoading_ ? [1,2,3,4,5].map((e,i)=><div key={i} style={{marginTop:18}}><CommunityQuestionSkeleton/></div>):
-<ScrollBar style={{maxHeight:"75vh"}}>
-            <div className="myContainer">
+      {isLoading_ ? [1, 2, 3, 4, 5].map((e, i) => <div key={i} style={{ marginTop: 18 }}><CommunityQuestionSkeleton /></div>) :
+        <ScrollBar style={{ maxHeight: "75vh" }}>
+          <div className="myContainer">
+            {!isLoading_ && data?.length <= 0 && !error && <div>
+              <>
+                <NoData>No question available</NoData>
+              </>
+            </div>}
 
+            {!isLoading_ && data?.length <= 0 && error && <div>
+              <>
+                <NoData image='/images/error.png'>{error}</NoData>
+              </>
+            </div>}
 
-            {!isLoading_  && data?.length <= 0 && !error && <div>
-<>
-<NoData>No question available</NoData>
-</>
-</div>}
-
-{!isLoading_  && data?.length<=0 && error && <div>
-<>
-<NoData image='/images/error.png'>{error}</NoData>
-</>
-</div>}
-
-
-
-{
-data?.map((e:any,i:number)=>{
-    
-return (<CommunityQuestion post={e} key={i}/>)
-
-})
-}
-</div>
-<br/><br/>
-</ScrollBar> 
-}
-        </div>
-    )
+            {data?.map((e: any, i: number) => {
+              return (<CommunityQuestion post={e} key={i} />);
+            })}
+          </div>
+          <br /><br />
+        </ScrollBar>
+      }
+    </div>
+  );
 }
 
 export default Main;
